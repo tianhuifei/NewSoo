1e72c8d0bca6eb9ed170ec49fa2a8824739dd811
wang <470533840@qq.com>
2017-05-10 11:17:59 +0800

    wxq

---
+++ html/申请试用/js/trial.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+$(".left-list li").click(function(){
+    $(this).addClass("active").children().children().addClass("circle_on");
+    $(this).siblings().removeClass("active").children().children().removeClass("circle_on");
+
+    var list_type=$(this).attr("data-type");
+    switch(list_type){
+        case "1":
+            $(".applyfor-trial").show().siblings().hide();
+            break;
+        case "2":
+            $(".forget-pass").show().siblings().hide();
+            break;
+    }
+
+})
